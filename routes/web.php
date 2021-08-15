2dfbb1f6feee2cd68c94815068bde592d47deab0
kurniawanedi <edikurniawan27@outlook.com>
2021-08-15 22:13:27 +0700

    learn routing

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/profile', function () {
+    $page = "Profile";
+    
+    return view('profile', [
+        'page' => $page
+    ]);
 });
